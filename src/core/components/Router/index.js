0db4d362a519c223e81e74479ab7c5ea7244919c
rodionkuhtysolveit <rodionkuhty@gmail.com>
2022-01-12 14:25:38 +0300

    make a redirect in router to /inputphonenumber page

--- src/core/components/Router/index.js
+++ src/core/components/Router/index.js
@@ -12,10 +12,9 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/inputphonenumber" element={<Login />} />
-      <Route path="/" element={<Home />} />
       <Route path="/pushauth" element={<PushAuth />} />
       <Route path="/altotpauth" element={<AltOtpAuth />} />
-      <Route render={() => <Navigate to="/" />} />
+      <Route path="" element={<Navigate to="/inputphonenumber" />} />
     </Routes>
   )
 }
